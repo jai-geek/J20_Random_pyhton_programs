e79512f72c25f53f81c9e1fc58b9da5d500e2643
Jai Krishna Chaubey <73540633+jai-geek@users.noreply.github.com>
2021-10-26 22:30:14 -0700

    Update DSA_pg_1.py

--- DSA_pg_1.py
+++ DSA_pg_1.py
@@ -1,7 +1,7 @@
 # reverse the array 
-def reverse(arr):      #function with argument arr
-    arr.reverse()      #used reverse inbuilt keyword with the array
-    return arr         #return the array
-arr=[1,2,3,4,5]        #input the list of array and stored in arr
-print(reverse(arr))    #print by calling the function with arg...
+def reverse(arr):     
+    arr.reverse()      
+    return arr        
+arr=[1,2,3,4,5]      
+print(reverse(arr))    
     
